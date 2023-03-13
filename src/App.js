d21db837821718ce2a8b816ea908fe033c90c0ed
unknown <balasdeep@gmail.com>
2023-03-13 22:19:08 +0530

    created instagram clone FE

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React,{useEffect, createContext, useReducer, useContext} from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom';
+import Home from './component/screen/Home';
+import Signin from './component/screen/Signin';
+import Profile from './component/screen/Profile';
+import Signup from './component/screen/Signup';
+import CreatePost from './component/screen/CreatePost';
+import UserProfile from './component/screen/UserProfile';
+import SubscribeUserPosts from './component/screen/SubscribeUserPosts';
+import {initialState, reducer} from './reducers/userReducer';
+ 
+export const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
+  const {state, dispatch} = useContext(UserContext);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    //console.log(user);
+    if(user){
+      dispatch({type: "USER", payload:user})
+      //history.push('/')
+    }else{
+      if(!history.location.pathname.startsWith('/reset'))
+        history.push('/signin')
+    }
+  },[])
+  return(
+    <Switch>   
+       <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/signin'>
+          <Signin/>
+        </Route>
+        <Route path='/signup'>
+          <Signup/>
+        </Route>
+        <Route exact path='/profile'>
+          <Profile/>
+        </Route>
+        <Route path='/create'>
+          <CreatePost/>
+        </Route>
+        <Route path='/profile/:userid'>
+          <UserProfile/>
+        </Route>
+        <Route path='/myfollowingpost'>
+          <SubscribeUserPosts/>
+        </Route>
+    </Switch>
+  )
+}
+
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+    <UserContext.Provider value={{state,dispatch}}>
+      <BrowserRouter>
+        <Navbar/>
+        <Routing/>
+      </BrowserRouter>
+    </UserContext.Provider>
+     
+    </div>
+  );
+}
+
+export default App;
